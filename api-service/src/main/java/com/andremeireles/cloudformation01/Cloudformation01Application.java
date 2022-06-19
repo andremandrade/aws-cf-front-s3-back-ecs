6987aa745229b2546c3ee056272337f8aa01fc7b
Andre Meireles <andre@crateus.ufc.br>
2022-06-19 18:22:50 -0300

    api service - building docker image

---
+++ api-service/src/main/java/com/andremeireles/cloudformation01/Cloudformation01Application.java
@@ -0,0 +1,13 @@
+package com.andremeireles.cloudformation01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cloudformation01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cloudformation01Application.class, args);
+	}
+
+}
---
+++ api-service/src/test/java/com/andremeireles/cloudformation01/Cloudformation01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andremeireles.cloudformation01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cloudformation01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
