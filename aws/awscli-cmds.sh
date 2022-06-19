9a2000baf5aee514cdde00ae06065eaf31cf864d
Andre Meireles <andre@crateus.ufc.br>
2022-06-19 15:22:23 -0300

    deploy webfrontend on s3

--- aws/awscli-cmds.sh
+++ aws/awscli-cmds.sh
@@ -6,3 +6,7 @@ aws cloudformation describe-stack-resource --stack-name aws-cloudformation01-tes
 aws cloudformation update-stack --stack-name aws-cloudformation01-test --template-body file://`pwd`/cloudformation-template.yml
 
 aws cloudformation describe-stack-resource --stack-name aws-cloudformation01-test --logical-resource-id WebSiteBucket --query StackResourceDetail.PhysicalResourceId --output text
+
+
+
+aws s3 sync ./web-frontend/dist/web-frontend/ s3://$S3_BUCKET_NAME
