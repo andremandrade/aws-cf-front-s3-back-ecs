99f57247d93499bfc1381e07ceb0d5f47d7a51e5
Andre Meireles <andre@crateus.ufc.br>
2022-07-06 18:32:08 -0300

    resolving conflict

--- aws/awscli-cmds.sh
+++ aws/awscli-cmds.sh
@@ -10,3 +10,13 @@ aws cloudformation describe-stack-resource --stack-name aws-cloudformation01-tes
 
 
 aws s3 sync ./web-frontend/dist/web-frontend/ s3://$S3_BUCKET_NAME
+
+aws ecr get-login-password --region us-west-2 --profile andre | docker login --username AWS --password-stdin 380095960694.dkr.ecr.us-west-2.amazonaws.com
+
+docker tag cf01-api-service:latest 380095960694.dkr.ecr.us-west-2.amazonaws.com/cf01-api-service:latest
+
+docker push 380095960694.dkr.ecr.us-west-2.amazonaws.com/cf01-api-service:latest
+
+aws ecs register-task-definition --family CF01ECSServiceAPI --execution-role-arn arn:aws:iam::380095960694:role/ecsTaskExecutionRole --network-mode awsvpc --container-definitions "{\"name\": \"CF01API\",\"image\":\"380095960694.dkr.ecr.us-west-2.amazonaws.com/cf01-api:21c109e\",\"cpu\": 512,\"memory\":1024,\"portMappings\":[{\"containerPort\": 8080}],\"essential\": true}" --cpu 1024 --memory 2048
+
+aws ecs update-service --cluster CF01ECSCluster --service CF01ECSServiceAPI --task-definition CF01ECSServiceAPI:2
